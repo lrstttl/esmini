b54aebc506ce4f1bd7cccda18dee9d29d6f09c57
climent jackey clement <cclement@volvocars.com>
2023-11-03 16:44:23 +0100

    working version 1

--- EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/DatLogger.cpp
+++ EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/DatLogger.cpp
@@ -31,6 +31,7 @@ void DatLogger::logPackage(PackageStructPos package )
 {
     if (data_file_.is_open())
     {
+
         PackageStructPos pos = getLatestPackage<PackageStructPos>(package.hdr.id);
         if (pos.pos.h == package.pos.h)
         {
@@ -102,17 +103,53 @@ T DatLogger::getLatestPackage(const int id) {
 
     // Find the package with the specified ID, searching backward from the end of the file.
     T package;
-    std::cout << "Complete Package Size: " << sizeof(T) << std::endl;
+    std::cout << "Given Package Size: " << sizeof(T) << std::endl;
     if (static_cast<int>(lastPointerPosition) < static_cast<int>(sizeof(T)) || isFirstEntry)
     {
+        data_file_.seekg(currentFilePointerPosition, std::ios::beg);
         std::streampos exitPointerPosition = data_file_.tellg();
         std::cout << "Pointer num: " << exitPointerPosition <<" while exit" << std::endl;
         return T();
     }
-
+    int searchingPackSize = 0;
     // std::cout << "Complete Package Size T: " << sizeof(T) << std::endl;
     while (data_file_.tellg() > 0) {
-        data_file_.seekg(-sizeof(T), std::ios::cur);
+
+        data_file_.seekg(-4, std::ios::cur);
+        searchingPackSize += 4;
+        std::cout << "Searching package size: " << searchingPackSize << " dynamic"<<std::endl;
+        std::streampos dynamic_position = data_file_.tellg();
+
+        // Check if the seekg() function was successful.
+        if (dynamic_position == std::streampos(-1)) {
+            std::cout << "pointer did not moved" << std::endl;
+        // Handle the error.
+        }
+
+        std::cout << "Pointer num: " << dynamic_position <<" dynamic" << std::endl;
+
+        // check furthur in case last package
+        if (data_file_.tellg() != 0)
+        {
+            // continue searching for two package end Read the pervious package end.
+            data_file_.read(reinterpret_cast<char*>(&package.package_end.end_of_package), 4);
+            std::cout << "Pointer num: " << data_file_.tellg() <<" read" << std::endl;
+            // // move back again
+            data_file_.seekg(-4, std::ios::cur);
+            std::cout << "Pointer num: " << data_file_.tellg()<<" corrected" << std::endl;
+
+            // continue searching till same size package as given package
+            if (package.package_end.end_of_package != sizeof(T)) {
+                searchingPackSize = 0;
+            continue;
+            }
+
+            std::cout << "---Package size matches with given package---- " << std::endl;
+
+        }
+        data_file_.seekg(-(sizeof(T)-4), std::ios::cur);
+
+        std::cout << "Pointer Num after matching size: " << data_file_.tellg() << std::endl;
 
         // Read the package ID.
         data_file_.read(reinterpret_cast<char*>(&package.hdr.id), sizeof(package.hdr.id));
@@ -121,6 +158,8 @@ T DatLogger::getLatestPackage(const int id) {
         if (package.hdr.id != id) {
         continue;
         }
+
+        std::cout << "---Package id matches with id package---- " << std::endl;
 
         // Read the package size.
         data_file_.read(reinterpret_cast<char*>(&package.hdr.size), sizeof(package.hdr.size));
@@ -138,7 +177,7 @@ T DatLogger::getLatestPackage(const int id) {
         data_file_.read(reinterpret_cast<char*>(&package), sizeof(package));
 
         // Read the package data.
-        std::cout << "---Package found---- " << std::endl;
+        std::cout << "---Package found with same id and size as given---- " << std::endl;
         std::cout << "Complete Package Size: " << sizeof(package) << std::endl;
         // std::cout << "Package id Size: " << sizeof(package.hdr.id) << std::endl;
         // std::cout << "Package size Size: " << sizeof(package.hdr.size) << std::endl;
--- EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/DatLogger.hpp
+++ EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/DatLogger.hpp
@@ -8,7 +8,7 @@ enum class PackageId {
     ODR_FILENAME = 14,
     OSC_BOUNDING_BOX = 15,
 };
-
+#pragma pack(push, 4)
 typedef struct
 {
     int id;
@@ -17,31 +17,43 @@ typedef struct
 
 typedef struct
 {
+    int end_of_package;
+} CommonPackageEnd;
+
+typedef struct
+{
     CommonPackageHeader hdr;
     unsigned int version;
+    CommonPackageEnd package_end;
 } PackageStructVersion;
 
-    typedef struct
-    {
-        double x;
-        double y;
-        double z;
-        double h;
-        double r;
-        double p;
-    } PackageStructPosition;
+typedef struct
+{
+    double x;
+    double y;
+    double z;
+    double h;
+    double r;
+    double p;
+} PackageStructPosition;
 
-    typedef struct
-    {
-        CommonPackageHeader hdr;
-        PackageStructPosition pos;
-    } PackageStructPos;
 
-    typedef struct
-    {
-        CommonPackageHeader hdr;
-        double time;
-    } PackageStructTime;
+typedef struct
+{
+    CommonPackageHeader hdr;
+    PackageStructPosition pos;
+    CommonPackageEnd package_end;
+} PackageStructPos;
+
+
+typedef struct
+{
+    CommonPackageHeader hdr;
+    double time;
+    CommonPackageEnd package_end;
+} PackageStructTime;
+
+#pragma pack(pop)
 
 class DatLogger {
 private:
@@ -54,6 +66,7 @@ public:
         data_file_.close();
     }
     bool isFirstEntry = true;
+    bool notFirstEnd = false;
 
     int init(std::string fileName);
 
@@ -65,5 +78,97 @@ public:
     T getLatestPackage(const int id);
 
 };
+
+//main.cpp back up
+
+#include <cstring>
+#include <iostream>
+
+#include "DatLogger.hpp"
+
+int main(int, char**){
+    int version = 2;
+    std::string odr_fileName = "someScenario.xosc";
+    double time_  = 12.20;
+    std::string fileName = "sim.dat";
+
+    double x = 1.0;
+    double y = 2.0;
+    double z = 3.0;
+    double h = 4.0;
+    double r = 5.0;
+    double p = 6.0;
+
+    float bb = 1.0;
+    float bb1 = 2.0;
+
+
+    DatLogger logger;
+    logger.init(fileName);
+    for (int i = 0; i < 8 ; i++)
+    {
+        if (i > 0)
+        {
+            logger.isFirstEntry = false;
+        }
+        if (i == 3)
+        {
+            version = 3;
+        }
+        if (i == 4)
+        {
+            h = 6.0;
+        }
+        PackageStructVersion packageVersion;
+        packageVersion.hdr.id = static_cast<int>(PackageId::VERSION); // version id
+        packageVersion.hdr.size = sizeof(version);
+        packageVersion.version = version;
+        packageVersion.package_end.end_of_package = sizeof(packageVersion);
+        std::cout << "------Version Package->Send----- " << std::endl;
+        // std::cout << "Complete Package Size: " << sizeof(packageVersion) << std::endl;
+        // std::cout << "Package id Size: " << sizeof(packageVersion.hdr.id) << std::endl;
+        // std::cout << "Package size Size: " << sizeof(packageVersion.hdr.size) << std::endl;
+        // std::cout << "Package content Size: " << sizeof(packageVersion.version) << std::endl;
+        // std::cout << "Package size end: " << sizeof(packageVersion.package_end) << std::endl;
+        // std::cout << "------------------------- " << std::endl;
+        logger.logPackage(packageVersion);
+        std::cout << "------------------------- " << std::endl;
+
+        PackageStructPos packagePos;
+        packagePos.hdr.id = static_cast<int>(PackageId::POSITIONS); // version id
+        packagePos.pos.x = x;
+        packagePos.pos.y = y;
+        packagePos.pos.z = z;
+        packagePos.pos.h = h;
+        packagePos.pos.r = r;
+        packagePos.pos.p = p;
+        packagePos.hdr.size = sizeof(packagePos.pos);
+        packagePos.package_end.end_of_package = sizeof(packagePos);
+        std::cout << "------Pos package->Send----- " << std::endl;
+        // std::cout << "Complete Package Size: " << sizeof(packagePos) << std::endl;
+        // std::cout << "Package id Size: " << sizeof(packagePos.hdr.id) << std::endl;
+        // std::cout << "Package size Size: " << sizeof(packagePos.hdr.size) << std::endl;
+        // std::cout << "Package content Size: " << sizeof(packagePos.pos) << std::endl;
+        // std::cout << "------------------------- " << std::endl;
+        logger.logPackage(packagePos);
+        std::cout << "------------------------- " << std::endl;
+
+        PackageStructTime packageTime;
+        packageTime.hdr.id = static_cast<int>(PackageId::TIME_SERIES); // version id
+        packageTime.hdr.size = sizeof(time_);
+        packageTime.time = 12.20 + i;
+        packageTime.package_end.end_of_package = sizeof(packageTime);
+        std::cout << "------Time Package->Send----- " << std::endl;
+        // // // std::cout << "Complete Package Size: " << sizeof(packageVersion) << std::endl;
+        // // // std::cout << "Package id Size: " << sizeof(packageVersion.hdr.id) << std::endl;
+        // // // std::cout << "Package size Size: " << sizeof(packageVersion.hdr.size) << std::endl;
+        // // // std::cout << "Package content Size: " << sizeof(packageVersion.version) << std::endl;
+        // // // std::cout << "------------------------- " << std::endl;
+        logger.logPackage(packageTime);
+        std::cout << "------------------------- " << std::endl;
+
+    }
+
+}
 
 
